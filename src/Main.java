31bce5950e567cc361a1e00816762a12c3ad734b
gibrann <giresa.ico@gmail.com>
2020-11-06 08:53:58 -0600

    adding mqtt publish subscribe

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import java.security.*;
 import java.security.spec.*;
 import java.sql.Timestamp;
 import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.GZIPOutputStream;
 
 import org.eclipse.paho.client.mqttv3.*;
@@ -34,7 +36,7 @@ public class Main {
         System.out.println("private: " + priv);
         System.out.println("public: " + pub);
 
-        String plaintext = "CT60_DEMOTENANTTEST01";
+        String plaintext = "CT60_CMDTESTGIB01";
 
         //...... sign
         Signature ecdsaSign = Signature.getInstance(ALGO);
@@ -71,16 +73,103 @@ public class Main {
         return result;
     }
 
-    public static void subscribe(){
+    public static void subscribeIotHub(){
         MqttClient mqttClient = null;
         try {
-            mqttClient = new MqttClient("tcp://onpremgibors.westus2.cloudapp.azure.com:1883", "clientId");
+            mqttClient = new MqttClient("ssl://caidc-qaiothub.azure-devices.net:8883", "7b1586db-9a59-4bf3-942d-a1a850ddce73");
+
+            MqttConnectOptions connOpts = new MqttConnectOptions(); // 83ebb1e7-f5d3-4c8d-8b4a-4344179bbd91 //mydevice: 7b1586db-9a59-4bf3-942d-a1a850ddce73
+            connOpts.setUserName("caidc-qaiothub.azure-devices.net/7b1586db-9a59-4bf3-942d-a1a850ddce73/?api-version=2018-06-30");
+            connOpts.setCleanSession(true);
+            connOpts.setPassword("SharedAccessSignature sr=caidc-qaiothub.azure-devices.net%2Fdevices%2F&sig=9dAgbJG7z1sUIlZKfPPDj55dcngaaCWfy%2FCDF%2BqBy9M%3D&se=1604678166&skn=device".toCharArray());
+            connOpts.setKeepAliveInterval(60 * 5);
+            mqttClient.connect(connOpts);
+
+            mqttClient.setCallback(new MqttCallback() {
+
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    String time = new Timestamp(System.currentTimeMillis()).toString();
+                    System.out.println("\nReceived a Message!" +
+                            "\n\tTime:    " + time +
+                            "\n\tTopic:   " + topic +
+                            "\n\tMessage: " + new String(message.getPayload()) +
+                            "\n\tQoS:     " + message.getQos() + "\n");
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                    System.out.println("Delivered event !");
+
+                }
+
+                public void connectionLost(Throwable cause) {
+                    System.out.println("Connection to Solace broker lost!" + cause.getMessage());
+                }
+            });
+            mqttClient.subscribe("devices/7b1586db-9a59-4bf3-942d-a1a850ddce73/messages/devicebound/#", 1);
+            System.out.println("subscribed");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendNewConnectionEventIotHub(byte[] message){
+        MqttClient mqttClient = null;
+        try {
+            mqttClient = new MqttClient("ssl://caidc-qaiothub.azure-devices.net:8883", "7b1586db-9a59-4bf3-942d-a1a850ddce73");
+
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setUserName("caidc-qaiothub.azure-devices.net/7b1586db-9a59-4bf3-942d-a1a850ddce73/?api-version=2018-06-30");
+            connOpts.setCleanSession(true);
+            connOpts.setPassword("SharedAccessSignature sr=caidc-qaiothub.azure-devices.net%2Fdevices%2F&sig=9dAgbJG7z1sUIlZKfPPDj55dcngaaCWfy%2FCDF%2BqBy9M%3D&se=1604678166&skn=device".toCharArray());
+            connOpts.setKeepAliveInterval(60 * 5);
+
+            mqttClient.connect(connOpts);
+
+
+            mqttClient.setCallback(new MqttCallback() {
+
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    String time = new Timestamp(System.currentTimeMillis()).toString();
+                    System.out.println("\nReceived a Message!" +
+                            "\n\tTime:    " + time +
+                            "\n\tTopic:   " + topic +
+                            "\n\tMessage: " + new String(message.getPayload()) +
+                            "\n\tQoS:     " + message.getQos() + "\n");
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                    System.out.println("Delivered newconnection event !");
+
+                }
+
+                public void connectionLost(Throwable cause) {
+                    System.out.println("Connection to Solace broker lost!" + cause.getMessage());
+                }
+            });
+            MqttMessage mqttMessage = new MqttMessage(message);
+            mqttMessage.setQos(1);
+            mqttMessage.setRetained(false);
+            mqttClient.publish("devices/7b1586db-9a59-4bf3-942d-a1a850ddce73/messages/events/", mqttMessage);
+            System.out.println("published");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void subscribeOnPrem(){
+        MqttClient mqttClient = null;
+
+        try { //wss://onpremgibors.westus2.cloudapp.azure.com:15679/ws
+            //tcp://onpremgibors.westus2.cloudapp.azure.com:1883
+            mqttClient = new MqttClient("tcp://tempoisunil45fta.westeurope.cloudapp.azure.com:1883", "clientId");
 
             MqttConnectOptions connOpts = new MqttConnectOptions();
 
             connOpts.setCleanSession(true);
-            connOpts.setPassword("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdpYm9ycy1rZXkifQ.eyJhdWQiOlsicmFiYml0bXEiLCJyYWJiaXRfY2xpZW50Il0sInNjb3BlIjpbInJhYmJpdG1xLnJlYWQ6Ki8qLyoiLCJyYWJiaXRtcS50YWc6YWRtaW5pc3RyYXRvciIsInJhYmJpdG1xLmNvbmZpZ3VyZToqLyovKiIsInJhYmJpdG1xLndyaXRlOiovKi8qIl0sImV4cCI6MTYwMjIwMjA1NX0.qw08HGsj-XX5rV26oBVD-K6FtXEBsuEPSl_enuhadNU".toCharArray());
-            connOpts.setKeepAliveInterval(60 * 5); // connOpts.setKeepAliveInterval(60 * 5);
+            connOpts.setPassword("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEyMy1rZXkifQ.eyJhdWQiOlsicmFiYml0bXEiLCJyYWJiaXRfY2xpZW50Il0sInNjb3BlIjpbInJhYmJpdG1xLnJlYWQ6Ki8qIiwicmFiYml0bXEudGFnOmFkbWluaXN0cmF0b3IiLCJyYWJiaXRtcS5jb25maWd1cmU6Ki8qLyoiLCJyYWJiaXRtcS53cml0ZToqLyovKiJdLCJleHAiOjE2MDQ1NDY1Mzl9._8gs9jKRHX2QJhlKt14LC9hBfhdbEmnh5v84bTBn2KI".toCharArray());
+            connOpts.setKeepAliveInterval(600 * 5); // connOpts.setKeepAliveInterval(60 * 5);
             mqttClient.connect(connOpts);
 
             mqttClient.setCallback(new MqttCallback() {
@@ -102,91 +191,60 @@ public class Main {
 
                 public void connectionLost(Throwable cause) {
                     System.out.println("Connection to Solace broker lost!" + cause.getMessage());
-//                    latch.countDown();
                 }
             });
-            mqttClient.subscribe("devices/321eaaf4-1c98-4bc5-8fbb-e099df7cacd5/messages/devicebound/#", 1);
+            mqttClient.subscribe("devices/628344b7-396c-4041-b95f-1d2a8284aff7/messages/devicebound", 1);
             System.out.println("subscribed");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void sentDataIothub(){
-
-        MqttClient mqttClient = null;
-        try {
-            mqttClient = new MqttClient("tcp://caidc-qaiothub.azure-devices.net:1883", "b4eab5a4-06b5-4bd6-9279-86cfa0a216f2");
-
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-
-            connOpts.setCleanSession(true);
-            connOpts.setPassword("".toCharArray());
-            connOpts.setKeepAliveInterval(60 * 5); // connOpts.setKeepAliveInterval(60 * 5);
-            mqttClient.connect(connOpts);
-
-            mqttClient.setCallback(new MqttCallback() {
-
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    String time = new Timestamp(System.currentTimeMillis()).toString();
-                    System.out.println("\nReceived a Message!" +
-                            "\n\tTime:    " + time +
-                            "\n\tTopic:   " + topic +
-                            "\n\tMessage: " + new String(message.getPayload()) +
-                            "\n\tQoS:     " + message.getQos() + "\n");
-                }
-
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-                    System.out.println("sent successfully!");
-
-                }
-
-                public void connectionLost(Throwable cause) {
-                    System.out.println("Connection to Solace broker lost!" + cause.getMessage());
-//                    latch.countDown();
-                }
-            });
-            MqttMessage message = new MqttMessage("hello".getBytes());
-            mqttClient.publish("devices/{device_id}/messages/events/", message);
-            System.out.println("published ");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public static byte[] compress(String data) throws IOException {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream(data.length());
+    public static byte[] getNewConnection(String serialNumber) throws IOException {
+        String event = "{\"Events\":[{\"BodyProperties\":[{\"Key\":\"batteryserialnumber\",\"Value\":\"PEPA162000502333\"},{\"Key\":\"BluetoothAddress\",\"Value\":\"00:10:20:CD:02:C2\"},{\"Key\":\"configurationnumber\",\"Value\":\"CT60-L1N-ASC210E\"},{\"Key\":\"securitypatchlevel\",\"Value\":\"2019-05-01\"},{\"Key\":\"CurrentWirelessConnection\",\"Value\":\"None\"},{\"Key\":\"WiFiMacAddress\",\"Value\":\"00:10:20:cd:02:c1\"},{\"Key\":\"timestamp\",\"Value\":\"2020-05-25T08:24:05.570+00:00\"},{\"Key\":\"ipaddress\",\"Value\":\"192.168.0.4\"},{\"Key\":\"deviceuniqueid\",\"Value\":\"CT60CMDTESTGIB01\"},{\"Key\":\"Imei\",\"Value\":\"Unavailable\"},{\"Key\":\"Meid\",\"Value\":\"Unavailable\"},{\"Key\":\"family\",\"Value\":\"Dubai\"},{\"Key\":\"BatteryLevel\",\"Value\":\"100\"},{\"Key\":\"name\",\"Value\":\"CT60CMDTESTGIB01\"},{\"Key\":\"gatewayid\",\"Value\":\"\"},{\"Key\":\"osinfo\",\"Value\":\"Android 7.1.1\"},{\"Key\":\"type\",\"Value\":\"MobileComputer\"},{\"Key\":\"connectedagentversion\",\"Value\":\"5.10.11.0046\"},{\"Key\":\"model\",\"Value\":\"CT60\"},{\"Key\":\"firmwareversion\",\"Value\":\"84.00.19-(0169)\"}],\"CreatedTime\":\"2020-05-25T13:59:18.838+05:30\",\"CreatorType\":\"EBICC R1\",\"EventType\":\"newconnectionevent\",\"GeneratorType\":\"CT60_CMDTESTGIB01\",\"Id\":\"1a3b34d1-f2c8-40a5-9261-496633569c21\",\"TargetId\":\"16:79:50:B2:6F:86\"}]}\n";
+        System.out.println(event);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(event.length());
         GZIPOutputStream gzip = new GZIPOutputStream(bos);
-        gzip.write(data.getBytes());
+        gzip.write(event.getBytes());
         gzip.close();
         byte[] compressed = bos.toByteArray();
         bos.close();
         return compressed;
     }
 
-    public static void main(String[] args) throws NoSuchProviderException, InvalidParameterSpecException {
-        subscribe();
-//        try {
-//            Main digiSig = new Main();
-//            JSONObject obj = digiSig.sender();
-//            boolean result = digiSig.receiver(obj);
-//            System.out.println("");
-//            System.out.println(result);
-//        } catch (NoSuchAlgorithmException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (InvalidAlgorithmParameterException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (InvalidKeyException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (UnsupportedEncodingException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (SignatureException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (InvalidKeySpecException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+    public static void generateKeys() throws NoSuchProviderException, InvalidParameterSpecException {
+        try {
+            Main digiSig = new Main();
+            JSONObject obj = digiSig.sender();
+            boolean result = digiSig.receiver(obj);
+            System.out.println("");
+            System.out.println(result);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidAlgorithmParameterException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeyException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SignatureException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeySpecException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args) throws NoSuchProviderException, InvalidParameterSpecException, IOException {
+//        generateKeys();
+
+        // Send event to iothub
+        byte[] message = getNewConnection("CT60CMDTESTGIB01");
+        sendNewConnectionEventIotHub(message);
+
+        // Subscribe to topic Iothub
+        subscribeIotHub();
+        // subscribe onprem
+//        subscribeOnPrem();
     }
 
 }
